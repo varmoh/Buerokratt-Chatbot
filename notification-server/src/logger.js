c2e2dd7cd3559cd242a3bf785f703ecf83efe157
Varmo <101868197+varmoh@users.noreply.github.com>
2024-08-08 13:53:59 +0300

    Create logger.js

---
+++ notification-server/src/logger.js
@@ -0,0 +1,18 @@
+// logger.js
+const { createLogger, format, transports } = require('winston');
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp(),
+    format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level}]: ${message}`;
+    })
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: 'app.log' })
+  ]
+});
+
+module.exports = logger;
