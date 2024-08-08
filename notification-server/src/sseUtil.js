b0130482dee1cf9e50e943bfc0b4fcc987258f84
Varmo <101868197+varmoh@users.noreply.github.com>
2024-08-08 13:59:10 +0300

    Update sseUtil.js

--- notification-server/src/sseUtil.js
+++ notification-server/src/sseUtil.js
@@ -1,15 +1,18 @@
 const { v4: uuidv4 } = require('uuid');
+const logger = require('./logger');  // Import the logger
 
 function buildSSEResponse({ res, req, buildCallbackFunction }) {
   addSSEHeader(req, res);
   keepStreamAlive(res);
   const connectionId = generateConnectionID();
   const sender = buildSender(res);
-  
+
+  logger.info(`SSE connection established with Connection ID: ${connectionId} for ${req.originalUrl}`);
+
   const cleanUp = buildCallbackFunction({ connectionId, sender });
 
   req.on('close', () => {
-    console.log('Client disconnected from SSE');
+    logger.info(`Client with Connection ID: ${connectionId} disconnected from SSE`);
     cleanUp?.();
   });
 }
@@ -25,26 +28,39 @@ function addSSEHeader(req, res) {
     'Access-Control-Allow-Credentials': true,
     'Access-Control-Expose-Headers': 'Origin, X-Requested-With, Content-Type, Cache-Control, Connection, Accept'
   });
+
+  logger.info(`SSE headers set for Connection ID: ${uuidv4()}`);
 }
 
 function extractOrigin(reqOrigin) {
   const corsWhitelist = process.env.CORS_WHITELIST_ORIGINS.split(',');
   const whitelisted = corsWhitelist.indexOf(reqOrigin) !== -1;
-  return whitelisted ? reqOrigin : '*';
+  const origin = whitelisted ? reqOrigin : '*';
+
+  logger.info(`Extracted origin: ${origin} (whitelisted: ${whitelisted})`);
+  return origin;
 }
 
 function keepStreamAlive(res) {
   res.write('');
+  logger.debug('Stream kept alive with an empty message');
 }
 
 function generateConnectionID() {
   const connectionId = uuidv4();
-  console.log(`New client connected with connectionId: ${connectionId}`);
+  logger.info(`New client connected with Connection ID: ${connectionId}`);
   return connectionId;
 }
 
 function buildSender(res) {
-  return data => res.write(`data: ${JSON.stringify(data)}\n\n`);
+  return data => {
+    try {
+      logger.info(`Sending data to client: ${JSON.stringify(data)}`);
+      res.write(`data: ${JSON.stringify(data)}\n\n`);
+    } catch (error) {
+      logger.error('Error sending data:', error);
+    }
+  };
 }
 
 module.exports = {
